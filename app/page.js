4acbc60fc4dc0b9e9d63882c3e684e8a3d26eddf
hana_milad <89672587+hanamilad@users.noreply.github.com>
2024-06-01 01:26:01 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,15 @@
-"use client"
-import React,{useState} from "react";
-
-
-
+"use client";
+import React, { useState } from "react";
 
 export default function Home() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState("");
   const [showForm, setShowForm] = useState(false);
-
-
+  const [draggingTask, setDraggingTask] = useState(null);
 
   const handleAddTask = () => {
     setShowForm(true);
-  };  
+  };
 
   const handleInputChange = (e) => {
     setNewTask(e.target.value);
@@ -21,23 +17,38 @@ export default function Home() {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    if (newTask.trim() !== '') {
-      setTasks([...tasks, { id: tasks.length + 1, name: newTask, status: 'todo' }]);
-      setNewTask('');
+    if (newTask.trim() !== "") {
+      setTasks([
+        ...tasks,
+        { id: tasks.length + 1, name: newTask, status: "todo" },
+      ]);
+      setNewTask("");
       setShowForm(false);
     }
   };
 
   const handleDragStart = (e, id) => {
-    e.dataTransfer.setData('taskId', id);
+    e.dataTransfer.setData("taskId", id);
+    setDraggingTask(id);
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
+  const handleTouchStart = (e, id) => {
+    setDraggingTask(id);
   };
 
-  const handleDrop = (e, status) => {
-    const taskId = e.dataTransfer.getData('taskId');
+  const handleTouchEnd = (e) => {
+    const target = document.elementFromPoint(
+      e.changedTouches[0].clientX,
+      e.changedTouches[0].clientY
+    );
+    if (target && target.dataset && target.dataset.status) {
+      const status = target.dataset.status;
+      handleDrop(draggingTask, status);
+    }
+    setDraggingTask(null);
+  };
+
+  const handleDrop = (taskId, status) => {
     const updatedTasks = tasks.map((task) => {
       if (task.id === parseInt(taskId)) {
         return { ...task, status };
@@ -47,79 +58,121 @@ export default function Home() {
     setTasks(updatedTasks);
   };
 
-
   return (
     <main className="">
-    <div  className="text-center shadow-md p-3 ">
-      <h><span className="text-white bg-black rounded-md p-2 mr-2 ">Do</span>TO DO APP</h>
-    </div>
-    <div className="text-center  mt-4">
-      <button className="p-2 rounded-md bg-[#3c3d83] hover:opacity-80" onClick={handleAddTask}>Add New Task</button>
-    </div>
-    <div className="grid grid-cols-3 gap-2 mt-9 max-[900px]:grid-cols-1  ">
-      <div className=" bg-[#3c3d83] rounded-lg p-4 " onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'todo')}>
-        <h>To Do</h>
-  {
-  tasks
-  .filter((task) => task.status === 'todo')
-  .map((task) => (
-    <div
-      key={task.id}
-      className="bg-white text-black mt-3 rounded-lg p-2 cursor-pointer"
-      draggable
-      onDragStart={(e) => handleDragStart(e, task.id)}
-    >
-      {task.name}
-    </div>
-  ))}
+      <div className="text-center shadow-md p-3">
+        <h>
+          <span className="text-white bg-black rounded-md p-2 mr-2">Do</span>TO
+          DO APP
+        </h>
       </div>
-      <div className=" bg-[#3c3d83] rounded-lg p-4 "  onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'progress')}>
-        <h>Progression</h>
-{
-  tasks
-  .filter((task) => task.status === 'progress')
-  .map((task) => (
-    <div
-      key={task.id}
-      className="bg-black text-white mt-3 rounded-lg p-2 cursor-pointer"
-      draggable
-      onDragStart={(e) => handleDragStart(e, task.id)}
-    >
-      {task.name}
-    </div>
-  ))}
+      <div className="text-center mt-4">
+        <button
+          className="p-2 rounded-md bg-[#3c3d83] hover:opacity-80"
+          onClick={handleAddTask}
+        >
+          Add New Task
+        </button>
       </div>
-      <div className=" bg-[#3c3d83] rounded-lg p-4 " onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'done')}>
-        <h>Done</h>
-{
-  tasks
-  .filter((task) => task.status === 'done')
-  .map((task) => (
-    <div  
-      key={task.id}
-      className="bg-green-700 text-white mt-3 rounded-lg p-2 cursor-pointer"
-      draggable
-      onDragStart={(e) => handleDragStart(e, task.id)}
-    >
-      {task.name}
-    </div>
-  ))}
+      <div className="grid grid-cols-3 gap-2 mt-9 max-[900px]:grid-cols-1">
+        <div
+          className="bg-[#3c3d83] rounded-lg p-4"
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => {
+            e.preventDefault();
+            handleDrop(e.dataTransfer.getData("taskId"), "todo");
+          }}
+          data-status="todo"
+        >
+          <h>To Do</h>
+          {tasks
+            .filter((task) => task.status === "todo")
+            .map((task) => (
+              <div
+                key={task.id}
+                className="bg-white text-black mt-3 rounded-lg p-2 cursor-pointer"
+                draggable
+                onDragStart={(e) => handleDragStart(e, task.id)}
+                onTouchStart={(e) => handleTouchStart(e, task.id)}
+                onTouchEnd={handleTouchEnd}
+              >
+                {task.name}
+              </div>
+            ))}
+        </div>
+        <div
+          className="bg-[#3c3d83] rounded-lg p-4"
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => {
+            e.preventDefault();
+            handleDrop(e.dataTransfer.getData("taskId"), "progress");
+          }}
+          data-status="progress"
+        >
+          <h>Progression</h>
+          {tasks
+            .filter((task) => task.status === "progress")
+            .map((task) => (
+              <div
+                key={task.id}
+                className="bg-black text-white mt-3 rounded-lg p-2 cursor-pointer"
+                draggable
+                onDragStart={(e) => handleDragStart(e, task.id)}
+                onTouchStart={(e) => handleTouchStart(e, task.id)}
+                onTouchEnd={handleTouchEnd}
+              >
+                {task.name}
+              </div>
+            ))}
+        </div>
+        <div
+          className="bg-[#3c3d83] rounded-lg p-4"
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => {
+            e.preventDefault();
+            handleDrop(e.dataTransfer.getData("taskId"), "done");
+          }}
+          data-status="done"
+        >
+          <h>Done</h>
+          {tasks
+            .filter((task) => task.status === "done")
+            .map((task) => (
+              <div
+                key={task.id}
+                className="bg-green-700 text-white mt-3 rounded-lg p-2 cursor-pointer"
+                draggable
+                onDragStart={(e) => handleDragStart(e, task.id)}
+                onTouchStart={(e) => handleTouchStart(e, task.id)}
+                onTouchEnd={handleTouchEnd}
+              >
+                {task.name}
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
-    {
-      showForm && (
-      <form  onSubmit={handleFormSubmit} className="bg-white p-3 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-6   rounded-lg  text-black ">
-      <h1 className="font-bold">Add New Task</h1>
-      <div>
-        <label className="block">Title</label>
-        <input type="text" value={newTask}
-            onChange={handleInputChange} className="border" placeholder="Titel" autoFocus  />
-      </div>
-       <button  className="text-blue-950 bg-blue-400 p-2 rounded-lg">Add</button>
-      </form>
-     )
-    }
+      {showForm && (
+        <form
+          onSubmit={handleFormSubmit}
+          className="bg-white p-3 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-6 rounded-lg text-black"
+        >
+          <h1 className="font-bold">Add New Task</h1>
+          <div>
+            <label className="block">Title</label>
+            <input
+              type="text"
+              value={newTask}
+              onChange={handleInputChange}
+              className="border"
+              placeholder="Titel"
+              autoFocus
+            />
+          </div>
+          <button className="text-blue-950 bg-blue-400 p-2 rounded-lg">
+            Add
+          </button>
+        </form>
+      )}
     </main>
   );
 }
-
